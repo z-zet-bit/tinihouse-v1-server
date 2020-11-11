c51fc80f75ae9c671657a4e5b19c6cecd338ce49
Guenther <guenther.zoppelt@gmail.com>
2020-11-11 16:37:26 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+[this course](https://www.newline.co/courses/the-newline-guide-to-building-your-first-graphql-server-with-node-and-typescript/welcome
+)
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { listings, Listing } from './listings'
+
+const app = express()
+const port = 9000
+
+app.use(bodyParser.json())
+
+app.get('/', (_req, res) => res.send('Hello World!'))
+
+app.get('/listings', (_req, res) => res.send(listings) )
+
+app.post('delete-listing', (req, res) => {
+    const id: string = req.body.id
+    for (let i = 0; i<listings.length; i++) {
+        if (listings[i].id === id) {
+            return res.send(listings.splice(i, 1))
+        }
+    }
+    return res.send('faile to delete listing')
+})
+
+
+app.listen(port)
+console.log(`[app]: http://localhost:${port}`)
+
+
