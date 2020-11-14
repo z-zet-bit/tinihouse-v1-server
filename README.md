ea46c15a02c54d241f93ed6eae785c3f26d8fa31
Guenther <z.zet.bit@gmail.com>
2020-11-14 02:12:53 +0000

    end of module 3 and the free part

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 [this course](https://www.newline.co/courses/the-newline-guide-to-building-your-first-graphql-server-with-node-and-typescript/welcome
 )
+
+[at least until it comes to the pricing of £ 291.27](https://www.newline.co/tinyhouse#pricing)
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express from 'express'
-import bodyParser from 'body-parser'
-import { listings, Listing } from './listings'
+import { ApolloServer } from 'apollo-server-express'
+//import bodyParser from 'body-parser'
+//import { listings, Listing } from './listings'
+//import { schema } from './graphqSchema' 
+import { typeDefs, resolvers } from './graphql'
 
 const app = express()
 const port = 9000
 
+const server = new ApolloServer({ typeDefs, resolvers }) //({ schema })
+server.applyMiddleware({ app, path: '/api'})
+
+
+
+
+/* to be replaced by graphql
 app.use(bodyParser.json())
-
 app.get('/', (_req, res) => res.send('<h1>tinyhouse-v1-server</h1>'))
-
 app.get('/listings', (_req, res) => res.send(listings) )
-
 app.post('/delete-listing', (req, res) => {
     const id: string = req.body.id
     for (let i = 0; i < listings.length; i++) {
@@ -20,6 +27,7 @@ app.post('/delete-listing', (req, res) => {
     }
     return res.send('faile to delete listing')
 })
+*/
 
 
 app.listen(port)
